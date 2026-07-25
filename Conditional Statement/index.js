469e1fc110888811866e79db11a8947c9af759f4
Tannu Singh <tannusingh252005@gmail.com>
2026-07-25 14:24:04 +0530

    first commit

---
+++ Conditional Statement/index.js
@@ -0,0 +1,12 @@
+// console.log(" this is our conditional tutorial ");
+// console.log(" this is our conditional tutorial2 ");
+// console.log(" this is our conditional tutorial3 ");
+// console.log(" this is our conditional tutorial4 ");
+// console.log(" this is our conditional tutorial 5");
+//  let  a = 45;
+//  if(a >= 18) {
+// console.log("this is a drive");
+//  }
+//  else{
+//     console.log("this is not drive");
+//  }
