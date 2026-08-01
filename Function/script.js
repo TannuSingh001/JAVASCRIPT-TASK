e20c1d2ca01cd8ea8a7566e0b6653a5d3ccbceaf
Tannu Singh <tannusingh252005@gmail.com>
2026-08-01 17:27:28 +0530

    Practise

---
+++ Function/script.js
@@ -0,0 +1,17 @@
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+
+// greet("tannu");
+// greet("shivani");
+// let num = [1,2,3,4,5];
+
+// let sum = 0;
+
+// for(let i = 0; i < num.length; i++){
+//     sum += num[i];
+// }
+
+// console.log(sum);
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+arrayName[index] = newValue;
---
+++ Variable/const.js
@@ -0,0 +1,18 @@
+const colleage ="JIET";
+console.log(colleage);
+colleage ="IIT delhi"
+console.log(colleage);
+const marks = [80, 90];
+
+marks.push(95);
+
+console.log(marks);
+// DOM Manipulation
+// ✅ Events
+// ✅ Functions
+// ✅ Arrays
+// ✅ Loops
+// ✅ Conditions
+// ✅ Operators
+// ✅ Template Literals
+// ✅ CSS Manipulation through JavaScript
