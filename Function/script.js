578c312e7358ba8aa54ad1e1677bd11dac9fcb16
Tannu Singh <tannusingh252005@gmail.com>
2026-08-01 17:35:13 +0530

    Practise

--- Function/script.js
+++ Function/script.js
@@ -13,5 +13,13 @@
 // }
 
 // console.log(sum);
-let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
-arrayName[index] = newValue;
+// let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+// arrayName[index] = newValue;
+let numbers =[10, 20, 30, 20, 40, 10, 50, 30]
+let uniqueNumbers = [];
+for (let i=0;i<numbers.length;i++){
+    if(!uniqueNumbers.includes(numbers[i])){
+        uniqueNumbers.push(numbers[i]);
+    }
+}
+console.log(uniqueNumbers);
