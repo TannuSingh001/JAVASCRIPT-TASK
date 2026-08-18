dc8fa2785b9890b7510b3f9580f9405cd847a177
Tannu Singh <tannusingh252005@gmail.com>
2026-08-18 16:24:54 +0530

    javascript

--- String/string.js
+++ String/string.js
@@ -19,6 +19,25 @@
 // console.log(message.replace('i', 'hii'));
 // const message = "i am i";
 // console.log(message.replace('i', 'hii'));
-const a = "hello i am tannu";
-const ReplaceALL = a.replaceAll
-console.log(a.replaceAll('hello i am tannu', 'hii,good morinig'))
+// const a = "hello i am tannu";
+// const ReplaceALL = a.replaceAll
+// console.log(a.replaceAll('hello i am tannu', 'hii,good morinig'))
+// const message = " iam tannu";
+// console.log (message);
+
+// const finalmessage = "this is a tsanshu"
+// console.log( message.concat(finalmessage))
+// padstart
+// const lastfourdigit = '78654'
+// console.log('****'+lastfourdigit)
+// console.log(lastfourdigit+'*8****')
+// const a = "67";
+// console.log(a.padStart(16, '*'));
+// const a = "this is a hero"
+// console.log(a.charCodeAt())
+// const name = "she is a anurag";
+// console.log(name.split(' '));
+const name = "Tannu";
+const age = 21;
+
+console.log(`My name is ${name} and my age is ${age}`);
