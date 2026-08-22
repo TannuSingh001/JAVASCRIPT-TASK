6c3e34e8461185a7fbf99470c2904b999d370b54
Tannu Singh <tannusingh252005@gmail.com>
2026-08-22 14:56:26 +0530

    Javacript

--- Conditional Statement/Terniary.js
+++ Conditional Statement/Terniary.js
@@ -1,3 +1,48 @@
-true ? 'Anurag' :100 ;
-false ? 'shiv' :100;
-5>8 ? 'relay':199;
+// true ? 'Anurag' :100 ;
+// false ? 'shiv' :100;
+// 5>8 ? 'relay':199;
+// let age = 20;
+
+// let result = age >= 18 ? "Eligible to Vote" : "Not Eligible";
+
+// console.log(result);
+// let amount = 6000;
+
+// let discount = amount >= 5000 ? 10 : 5;
+
+// let finalAmount = amount - (amount * discount / 100);
+
+// console.log("Discount:", discount + "%");
+// console.log("Final Amount:", finalAmount);
+// let a = 45;
+// let b = 78;
+// let c = 62;
+
+// let largest =
+//     a > b
+//         ? (a > c ? a : c)
+//         : (b > c ? b : c);
+
+// console.log("Largest:", largest);
+// let salary = 45000;
+// let experience = 4;
+
+// let bonus =
+//     experience >= 5 ? salary * 0.20 :
+//     experience >= 3 ? salary * 0.10 :
+//     experience >= 1 ? salary * 0.05 :
+//     0;
+
+// console.log("Bonus: ₹" + bonus);
+// console.log("Total Salary: ₹" + (salary + bonus));
+// let amount = 1800;
+// let isPrimeMember = false;
+
+// let delivery =
+//     isPrimeMember ? 0 :
+//     amount >= 2000 ? 0 :
+//     amount >= 1000 ? 50 :
+//     100;
+
+// console.log("Delivery Charge: ₹" + delivery);
+// console.log("Final Amount: ₹" + (amount + delivery));
